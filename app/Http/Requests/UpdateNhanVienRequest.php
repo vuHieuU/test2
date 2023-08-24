5c447623200c2dc00eab9ab7d12431bd02638e5d
vutrunghieu <vuhieu13102003@gmail.com>
2023-08-24 21:58:23 +0700

    format tel

--- app/Http/Requests/UpdateNhanVienRequest.php
+++ app/Http/Requests/UpdateNhanVienRequest.php
@@ -24,7 +24,7 @@ class UpdateNhanVienRequest extends FormRequest
         return [
             'name' => 'required',
             'email' => 'required|email',
-            'tel' => 'required|regex:/^[0-9]/|max:14',
+            'tel' => 'required|max:14|regex:/^\d{4}-\d{4}-\d{4}$/',
         ];
     }
 }
--- app/Http/Requests/nhanVienRequest.php
+++ app/Http/Requests/nhanVienRequest.php
@@ -24,7 +24,7 @@ class nhanVienRequest extends FormRequest
         return [
             'name' => 'required',
             'email' => 'required|email|unique:nhan_viens',
-            'tel' => 'required|max:14|regex:/^[0-9]/',
+            'tel' => 'required|max:14|regex:/^\d{4}-\d{4}-\d{4}$/',
         ];
     }
 }
